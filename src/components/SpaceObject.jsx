6e58947cb74eedcdec9e8916e703a8443947faf6
daytonq <uravolkov28@gmail.com>
2025-04-05 02:24:05 +0300

    fix: adjusting coord to fit zoom

--- src/components/SpaceObject.jsx
+++ src/components/SpaceObject.jsx
@@ -1,16 +1,14 @@
 import React from 'react';
 import { Graphics } from '@pixi/react';
 
-function SpaceObject({ pos, canvasSize, scale, position}) {
-  const x = (canvasSize.width / 2 - canvasSize.width * 0.15 + pos.x) / scale;
-  const y = (canvasSize.height / 2 + pos.y) / scale;
+function SpaceObject({ pos} ) {
   return (
     <Graphics
       draw={(g) => {
         g.clear();
         g.lineStyle(0);
         g.beginFill(0xffffff);
-        g.drawCircle(x, y, pos.radius * scale);
+        g.drawCircle(pos.x, pos.y, pos.radius);
         g.endFill();
       }}
     />
